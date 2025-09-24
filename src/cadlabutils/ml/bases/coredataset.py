1e024170900775c15938321f2e64da15fa536e40
ikestar99 <77869090+ikestar99@users.noreply.github.com>
2025-09-24 00:04:02 -0700

    Debugging optimizer dtype mismatch

--- src/cadlabutils/ml/bases/coredataset.py
+++ src/cadlabutils/ml/bases/coredataset.py
@@ -277,9 +277,11 @@ class CoreDataset(Dataset):
                 print("padre fucked up yo")
                 print(item[self._INDEX])
                 print(self.parent.meta.iloc[[item[self._INDEX]]].reset_index(drop=False).iloc[0])
-                import sys; sys.exit()
+
 
         item = item if self.parent is None else self.parent[item[self._INDEX]]
+        import sys;
+        sys.exit()
         return item
 
     def __add__(
