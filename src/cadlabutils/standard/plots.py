85d7ea3368e8b7ccb7c9be4b78cade6c93c27e2d
ikestar99 <77869090+ikestar99@users.noreply.github.com>
2026-08-25 16:59:53 -0700

    Updated PCA

--- src/cadlabutils/standard/plots.py
+++ src/cadlabutils/standard/plots.py
@@ -10,16 +10,12 @@ Created on Fri Apr 9 03:38:05 2021
 import io
 
 # 2. Third-party library imports
-import matplotlib
 import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
-from matplotlib.ticker import MaxNLocator
-from mpl_toolkits.mplot3d import Axes3D
 import numpy as np
 import pandas as pd
 from PIL import Image
 import seaborn as sns
-from sklearn.neighbors import KernelDensity
+from scipy.stats import gaussian_kde
 
 
 # set style and colors
@@ -39,27 +35,41 @@ def style_ax(
         y_color: str = None,
         x_cross: float = 0,
         y_cross: float = 0,
+        x_round: int = None,
+        y_round: int = None,
         tick_size: int = 30,
         label_size: int = 25,
         label_weight: str = "bold",
         label_color: str = "black",
         line_width: int = 5,
         draw_yx: bool = False,
+        symmetric_bounds: bool = False
 ):
     for spine in ax.spines.values():
         spine.set(color=label_color, linewidth=line_width)
-    for axis, label, ticks, color, cross in (
-            ("x", x_label, x_ticks, x_color, y_cross),
-            ("y", y_label, y_ticks, y_color, x_cross)):
+    for axis, label, ticks, color, cross, p_10 in (
+            ("x", x_label, x_ticks, x_color, y_cross, x_round),
+            ("y", y_label, y_ticks, y_color, x_cross, y_round)):
         if label is not None:
             getattr(ax, f"set_{axis}label")(
                 label, fontsize=label_size, fontweight=label_weight,
                 color=label_color)
+        if ticks is None and p_10 is not None:
+            p_min, p_max = getattr(ax.dataLim, f"interval{axis}")
+            _p = np.max(np.abs((p_min, p_max)))
+            p_min, p_max = (-_p, _p) if symmetric_bounds else (p_min, p_max)
+            p_mid = np.floor(
+                (p_min + p_max) / 2 * (10 ** p_10)) / (10 ** p_10)
+            p_mid = np.unique([p_mid, 0 if p_min < 0 < p_max else p_mid])
+            ticks = (
+                np.floor(p_min * (10 ** p_10)) / (10 ** p_10), *p_mid,
+                np.ceil(p_max * (10 ** p_10)) / (10 ** p_10))
+
         if ticks is not None:
             getattr(ax, f"set_{axis}lim")(ticks[0], ticks[-1])
             getattr(ax, f"set_{axis}ticks")(ticks)
             bounds = getattr(ax, f"get_{axis}lim")()
-            if bounds[0] < cross < bounds[1]:
+            if cross is not None and bounds[0] < cross < bounds[1]:
                 l, s = ("h", "bottom") if axis == "y" else ("v", "left")
                 getattr(ax, f"ax{l}line")(
                     cross, color="black", linewidth=line_width, zorder=0)
@@ -100,35 +110,39 @@ def ridgeplot(
         hue: str,
         hue_order: list,
         palette: dict,
-        clip: tuple,
+        clip: tuple = None,
         bins: int = 100,
         lw: float = 5,
-        spacing: float = 0.8
+        spacing: float = 0.7,
+        padding: float = 0.1,
 ):
     lines = {}
+    clip = np.array([data[x].min(), data[x].max()] if clip is None else clip)
+    _x = np.linspace(
+        *(clip + (np.ptp(clip) * np.array([-padding, padding]))), bins)
     for h, h_df in data.groupby(hue):
-        _y, _x = np.histogram(h_df[x], bins=bins)
-        # _x = np.linspace(clip[0], clip[1], bins)
-        # _y = h_df[x].dropna().to_numpy()
-        # kde = KernelDensity(
-        #     kernel="gaussian", bandwidth=0.2).fit(_y[:, None])
-        # _y = np.exp(kde.score_samples(_x[:, None]))
-        lines[h] = _x[:-1], _y / np.trapezoid(_y, _x[:-1])
+        lines[h] = gaussian_kde(h_df[x].dropna())(_x)
 
-    offset = max([lines[k][1].max() for k in lines]) * spacing
+    offset = max([lines[k].max() for k in lines]) * spacing
     peak = 0
     for i, key in enumerate(hue_order):
         if key not in lines:
             continue
 
-        _x, _y = lines[key]
+        _y = lines[key]
         _dy = offset * (len(hue_order) - 1 - i)
-        ax.plot(_x, _y + _dy, color=palette[key], linewidth=lw)
-        ax.fill_between(_x, _y + _dy, _dy, color=palette[key], alpha=0.2)
-        ax.plot(_x, [_dy] * len(_x), color="gray", linewidth=lw)
+        ax.plot(_x, _y + _dy, color="w", linewidth=5, zorder=i + 1)
+        ax.fill_between(_x, _y + _dy, _dy, color=palette[key], zorder=i + 1)
+        ax.plot(_x, [_dy] * len(_x), color="black", linewidth=2, zorder=i + 1)
         peak = max(peak, np.max(_y + _dy))
 
-    return ax, peak * 1.1
+        r_just = _x[np.argmax(_y)] <= np.sum(clip) / 2
+        ax.text(
+            x=clip[int(r_just)], y=_dy + (offset * 0.5), s=key,
+            color=palette[key], fontsize=25, ha="right" if r_just else "left",
+            va="center")
+
+    return ax, peak, (min(_x), max(_x))
 
 
 def fig_to_im(
